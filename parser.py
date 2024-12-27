f1c3e17573eb8705789287bdd6e647843995b736
nikolay <86667038+mrcirniko@users.noreply.github.com>
2024-12-27 22:37:10 +0300

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import re
+from repositories.actions import add_movie
+
+# Загрузка данных
+movies = pd.read_csv('./dataset/movies.csv')
+
+# Обработка каждой строки
+for _, row in movies.iterrows():
+    # Разделяем строку на movieId, title и genres
+    movieId = row['movieId']
+    full_title = row['title']
+    genres = row['genres']
+
+    # Выделяем дату из title (если есть)
+    match = re.search(r'\((\d{4})\)', full_title)
+    if match:
+        year = match.group(1)  # Год внутри скобок
+        title = full_title[:match.start()].strip()  # Название до скобок
+    else:
+        year = 1111
+        title = full_title.strip()  # Название целиком, если года нет
+
+    # Разделяем жанры по "|"
+    genres_list = genres.split('|')
+
+    # Вызываем функцию add_movie
+    add_movie(movieId, title, year, genres_list)
