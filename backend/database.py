c6dc5b9852a167117d817a9599de5e05a47049d5
Crane <113766015+CraneHere@users.noreply.github.com>
2024-12-27 11:08:17 +0300

    smth

--- backend/database.py
+++ backend/database.py
@@ -1,20 +1,11 @@
-import tenacity
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql+psycopg2://user:password@db:5432/dbname')
 
-@tenacity.retry(
-    wait=tenacity.wait_exponential(multiplier=1, min=4, max=10),
-    stop=tenacity.stop_after_attempt(5),
-    retry=tenacity.retry_if_exception_type(Exception),
-)
-def get_engine():
-    return create_engine(SQLALCHEMY_DATABASE_URL)
-
-engine = get_engine()
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
